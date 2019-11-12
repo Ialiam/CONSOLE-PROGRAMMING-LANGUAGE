d86c3834ee42da9e42a5210a477da62dec31d5f5
Iftikher Ahmed Liam <55353556+Ialiam@users.noreply.github.com>
2019-11-12 20:21:19 +0600

    Basic

---
+++ C++ basic/first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+ {
+
+
+
+     int age = 0;
+     cout << "hey arnob enter your age" <<endl;
+     cin >> age;
+
+     cout << "You entered :" << age << endl;
+
+     return 0;
+ }
